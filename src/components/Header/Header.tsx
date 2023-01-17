72984520639dc5c032ddbe28add8b71982e2e36e
eduardoromeu <eduardoromeueberts@gmail.com>
2023-01-17 18:40:24 -0300

    Redesigned header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,25 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { Button, Container, Navbar, Nav } from "react-bootstrap";
 
 export default function Header(){
     return (
         <Fragment>
             <header>
-                <Navbar bg="dark" variant="dark" expand="sm">
-                    <Container fluid>
-                        <Navbar.Brand as={Link} to="/">eduardoromeu.github.io</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="me-auto">
-                                <Nav.Link as={Link} to="/repos" className="text-decoration-underline">Repositories</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
+                <Navbar bg="dark" variant="dark">
+                    <Container fluid className="d-flex justify-content">
+                        <Navbar.Brand as="a" href="https://eduardoromeu.github.io/" className="btn btn-dark me-0">
+                            <i className="bi bi-arrow-left-short" />
+                        </Navbar.Brand>
+                        <Nav className="w-100">
+                            <Nav.Link as={Link} to="/repos" className="fw-semibold btn btn-dark w-100">
+                                <i className="bi bi-journal-bookmark me-2" />
+                                Repositories
+                            </Nav.Link>
+                            <Navbar.Brand as={Button} variant="dark" className="fw-semibold me-0">
+                                <i className="bi bi bi-gear-wide-connected" />
+                            </Navbar.Brand>
+                        </Nav>
                     </Container>
                 </Navbar>
             </header>
