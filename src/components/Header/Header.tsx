3015a400f8e85d5cb737249c329af7f31f198b49
eduardoromeu <eduardoromeueberts@gmail.com>
2023-01-18 18:47:23 -0300

    Fixed icons size in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,17 +7,21 @@ export default function Header(){
         <Fragment>
             <header>
                 <Navbar bg="dark" variant="dark" className="rounded-pill m-1">
-                    <Container fluid className="d-flex justify-content">
-                        <Navbar.Brand as="a" href="https://eduardoromeu.github.io/" className="btn btn-dark rounded-circle me-0">
-                            <i className="bi bi-arrow-left-short" />
+                    <Container fluid className="d-flex justify-content-between align-items-stretch">
+                        <Navbar.Brand as="a" href="https://eduardoromeu.github.io/" className="btn btn-dark rounded-circle me-0 d-flex align-items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left-short" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
+                            </svg>
                         </Navbar.Brand>
                         <Nav className="w-100">
                             <Nav.Link as={Link} to="/repos" className="fw-semibold btn btn-dark w-100 rounded-pill">
                                 <i className="bi bi-journal-bookmark me-2" />
                                 Repositories
                             </Nav.Link>
-                            <Navbar.Brand as={Button} variant="dark" className="fw-semibold me-0 rounded-circle" disabled>
-                                <i className="bi bi bi-gear-wide-connected" />
+                            <Navbar.Brand as={Button} variant="dark" className="fw-semibold me-0 rounded-circle d-flex align-items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-gear-wide-connected" viewBox="0 0 16 16">
+                                    <path d="M7.068.727c.243-.97 1.62-.97 1.864 0l.071.286a.96.96 0 0 0 1.622.434l.205-.211c.695-.719 1.888-.03 1.613.931l-.08.284a.96.96 0 0 0 1.187 1.187l.283-.081c.96-.275 1.65.918.931 1.613l-.211.205a.96.96 0 0 0 .434 1.622l.286.071c.97.243.97 1.62 0 1.864l-.286.071a.96.96 0 0 0-.434 1.622l.211.205c.719.695.03 1.888-.931 1.613l-.284-.08a.96.96 0 0 0-1.187 1.187l.081.283c.275.96-.918 1.65-1.613.931l-.205-.211a.96.96 0 0 0-1.622.434l-.071.286c-.243.97-1.62.97-1.864 0l-.071-.286a.96.96 0 0 0-1.622-.434l-.205.211c-.695.719-1.888.03-1.613-.931l.08-.284a.96.96 0 0 0-1.186-1.187l-.284.081c-.96.275-1.65-.918-.931-1.613l.211-.205a.96.96 0 0 0-.434-1.622l-.286-.071c-.97-.243-.97-1.62 0-1.864l.286-.071a.96.96 0 0 0 .434-1.622l-.211-.205c-.719-.695-.03-1.888.931-1.613l.284.08a.96.96 0 0 0 1.187-1.186l-.081-.284c-.275-.96.918-1.65 1.613-.931l.205.211a.96.96 0 0 0 1.622-.434l.071-.286zM12.973 8.5H8.25l-2.834 3.779A4.998 4.998 0 0 0 12.973 8.5zm0-1a4.998 4.998 0 0 0-7.557-3.779l2.834 3.78h4.723zM5.048 3.967c-.03.021-.058.043-.087.065l.087-.065zm-.431.355A4.984 4.984 0 0 0 3.002 8c0 1.455.622 2.765 1.615 3.678L7.375 8 4.617 4.322zm.344 7.646.087.065-.087-.065z"/>
+                                </svg>
                             </Navbar.Brand>
                         </Nav>
                     </Container>
